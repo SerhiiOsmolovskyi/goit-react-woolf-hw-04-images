af0e5acd75009207142d93dc9c3f980d8350192f
Serhii Osmolovskyi <109869085+SerhiiOsmolovskyi@users.noreply.github.com>
2024-02-22 11:48:53 -0700

    Initial commit

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,16 @@
+import styles from './ImageGalleryItem.module.css';
+
+export const ImageGalleryItem = ({ image, openModal }) => {
+  return (
+    <li className={styles.ImageGalleryItem}>
+      <img
+        src={image.webformatURL}
+        alt={image.tags}
+        className={styles.ImageGalleryItemImage}
+        onClick={() => openModal(image.largeImageURL)}
+      />
+    </li>
+  );
+};
+
+
